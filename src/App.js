7ebb8daae1a69babafe1b7df302b2358d12304be
Ariel <ariel.jimenez2025@gmail.com>
2020-06-03 23:12:00 -0400

    add app header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,66 @@
+/** @jsx jsx */
+// eslint-disable-next-line
 import React from 'react';
-
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import SearchIcon from '@material-ui/icons/Search';
+import { fade } from '@material-ui/core/styles';
+import MenuIcon from '@material-ui/icons/Menu';
+import Typography from '@material-ui/core/Typography';
+import InputBase from '@material-ui/core/InputBase';
+import { jsx, css } from '@emotion/core';
 
 function App() {
   return (
-    <div className="App">
-      <code>
-        See you in valhalla!
-      </code>
+    <div css={{
+      flexGrow: 1,
+    }}>
+      <AppBar
+        position="static"
+      >
+        <Toolbar>
+          <Typography
+            variant="h6"
+            noWrap
+            css={css`
+              width: 80%;
+            `}
+          >
+            Jirax
+          </Typography>
+          <div
+            css={css`
+              position: relative;
+              border-radius: 15px;
+              background-color: rgba(255,255,255, 0.15);
+              &:hover {
+                background-color: rgba(255,255,255, 0.25);
+              }
+            `}
+          >
+            <div css={css`
+              padding-left: 5px;
+              height: 100%;
+              position: absolute;
+              pointer-events: none;
+              display: flex;
+              align-items: center;
+              justify-content: center;
+            `}>
+              <SearchIcon />
+            </div>
+            <InputBase
+              placeholder="Search…"
+              css={css`
+                padding-left: 30px;
+                width: 100%;
+              `}
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          </div>
+        </Toolbar>
+      </AppBar>
     </div>
   );
 }
